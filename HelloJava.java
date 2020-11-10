a999271a20207164e873f8ffc0e1f61374f69404
nmtu3 <nmtu3@10.0.0.93>
2020-11-09 18:37:50 -0800

    not master branch

--- HelloJava.java
+++ HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("This is Java programming course");
+		System.out.println("GitHub Demo Project");
 	}
 
 }
